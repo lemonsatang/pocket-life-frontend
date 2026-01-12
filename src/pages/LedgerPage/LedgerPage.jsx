19fa68a1dd3bebdb3305b04847f4eeb565c83192
hr082022-rgb <hr082022@gmail.com>
2026-01-12 12:39:19 +0900

    가계부 프론트
    UI틀 생성하기

--- src/pages/LedgerPage/LedgerPage.jsx
+++ src/pages/LedgerPage/LedgerPage.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import PlaceholderPage from "../PlaceholderPage/PlaceholderPage";
 
 const LedgerPage = () => {
-  return <PlaceholderPage title="가계부 페이지" emoji="💰" />;
+  return (
+    <PlaceholderPage title="가계부" emoji="💰">
+      <div>
+        <p>가계부 화면 영역</p>
+      </div>
+    </PlaceholderPage>
+  );
 };
 
 export default LedgerPage;
