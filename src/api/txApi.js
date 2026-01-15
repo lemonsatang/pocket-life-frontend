fe1a2de3f9b586b055faae82627630df06911159
hr082022-rgb <hr082022@gmail.com>
2026-01-15 11:10:19 +0900

    가계부 api
    프론트 api 콘솔창 확인 코드 수정

--- src/api/txApi.js
+++ src/api/txApi.js
@@ -1,27 +1,29 @@
 import axios from "axios";
 
-/**
- * 가계부 전용 axios 인스턴스
- * - 모든 요청에 JWT 토큰을 자동으로 붙여줌
- * - 백엔드 SecurityConfig와 정확히 대응
- */
 const txApi = axios.create({
   baseURL: "http://localhost:8080/api/tx",
 });
 
-/**
- * 요청 인터셉터
- * - 요청 보내기 직전에 실행됨
- * - localStorage에 저장된 JWT를 그대로 Authorization 헤더에 추가
- */
-txApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+txApi.interceptors.request.use(
+  (config) => {
+    // 1. 저장된 토큰 꺼내기
+    const token = localStorage.getItem("token");
 
-  // 토큰이 있으면 그대로 헤더에 세팅
-  // if (token) {
-  //   config.headers.Authorization = token;
-  // }
-  // return config;
-});
+    // 2. 토큰이 있다면 규격에 맞게(Bearer ) 헤더에 넣어주기
+    if (token) {
+      // 만약 localStorage에 저장된 토큰에 이미 "Bearer "가 포함되어 있다면 그대로 넣고,
+      // 토큰 값만 들어있다면 아래처럼 `Bearer ${token}`으로 넣어줘야 합니다.
+      config.headers.Authorization = token.startsWith("Bearer ")
+        ? token
+        : `Bearer ${token}`;
+    }
+
+    // 3. ★가장 중요★ 수정한 설정(config)을 반드시 다음 단계로 넘겨줘야 합니다!
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default txApi;
